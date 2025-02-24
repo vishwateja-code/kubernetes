70b58d802ff062d84bd7762285c8c1a0c5d11b36
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2025-02-24 08:13:58 +0530

    Update test.sh

--- day-4-horizonatl-scaling/test.sh
+++ day-4-horizonatl-scaling/test.sh
@@ -1,3 +1,6 @@
-for j in {1..10}; do  for i in {1..1000}; do
-    curl -s -o /dev/null -w "%{http_code}\n" http://a0fbf831ca4a94936954790ef7a89bc4-104519041.ap-south-1.elb.amazonaws.com/ &  done
-  waitdone
+for j in {1..10}; do  
+    for i in {1..1000}; do  
+        curl -s -o /dev/null -w "%{http_code}\n" http://a48d1d51f2945469da6ffc19238aa8ad-1307616002.ap-south-1.elb.amazonaws.com/ &  
+    done  
+    wait  # Wait for all background curl processes to finish before next iteration
+done
